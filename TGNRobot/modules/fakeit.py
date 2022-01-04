46a128e8336f58c55bd121ec8ddffa4323186dca
zero <94368175+zerosquad13@users.noreply.github.com>
2022-01-04 23:54:51 +0700

    Update fakeit.py

--- TGNRobot/modules/fakeit.py
+++ TGNRobot/modules/fakeit.py
@@ -44,7 +44,7 @@ async def _(event):
             with open("FRIDAYOT.jpg", "wb") as f:
                 f.write(response.content)
 
-        captin = f"Fake Image powered by @GodFatherSupport."
+        captin = f"Fake Image powered by @lifeinsinn."
         fole = "FRIDAYOT.jpg"
         await tbot.send_file(event.chat_id, fole, caption=captin)
         await event.delete()
